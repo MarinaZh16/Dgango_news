fee88417bf6623fe3bf52cccd1913b2c81fae582
MarinaZh16 <msahno16@gmail.com>
2020-06-16 15:26:15 +0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -32,16 +32,15 @@ def login(request):
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(username=cd['username'], password=cd['password'])
-            if user is None:
-                return HttpResponse('Invalid login')
-            else:
+            if user is not None:
                 if user.is_active:
                     login_(request, user)
                     return redirect('/')
                 else:
                     return HttpResponse('Disabled account')
-
+            else:
+                return HttpResponse('Invalid login')
     else:
         form = LoginForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'users/login.html', {'form': form})
 
